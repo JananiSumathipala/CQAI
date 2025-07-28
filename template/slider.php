cf64f0c4e1d129b58b95e91fb6b6959612e1f334
kalpanairosh <kalpanairosh@gmail.com>
2025-07-28 09:21:24 +0530

    logo and img

--- template/slider.php
+++ template/slider.php
@@ -4,24 +4,24 @@
             <img src="assets/img/logo/qai-logo.png" alt="Command Quality Assurance Inspectorate Logo" class="logo">
         </div> -->
         <div class="swiper-wrapper">
-            <div class="swiper-slide">
+            <!-- <div class="swiper-slide">
                 <img src="assets/img/slider/one.jpg" alt="Quality Inspection">
                 <div class="slide-caption">
                     <h5>Ensuring Excellence in Every Operation</h5>
                 </div>
-            </div>
+            </div> -->
             <div class="swiper-slide">
                 <img src="assets/img/slider/four.jpg" alt="Our Team">
                 <div class="slide-caption">
                     <h5>Dedicated Team of Quality Professionals</h5>
                 </div>
             </div>
-            <div class="swiper-slide">
+            <!-- <div class="swiper-slide">
                 <img src="assets/img/slider/three.jpg" alt="Innovation">
                 <div class="slide-caption">
                     <h5>Innovative Solutions for Quality Assurance</h5>
                 </div>
-            </div>
+            </div> -->
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
