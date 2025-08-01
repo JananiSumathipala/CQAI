3b11110ecb1c32275313e6e59ce1f45048cc8130
Chathushka21 <chathuskakahawala@gmail.com>
2025-08-01 17:28:22 +0530

    services.php

--- services.php
+++ services.php
@@ -1,7 +1,6 @@
 <?php include 'template/head.php'; ?>
 
 <body>
-
     <?php
     include 'template/header.php';
     ?>
@@ -15,7 +14,6 @@
                 </div>
             </h3>
         </div>
-
         <div class="row">
             <div class="col-lg-2">
                 <!-- Nav Tabs -->
@@ -29,13 +27,13 @@
                     <li class="nav-item">
                         <a class="nav-link" id="structure-tab" data-bs-toggle="pill" href="#structure" role="tab">Feedback Report</a>
                     </li>
-                     <li class="nav-item">
+                    <li class="nav-item">
                         <a class="nav-link" id="structure-tab" data-bs-toggle="pill" href="#structure" role="tab">R&D</a>
                     </li>
-                     <li class="nav-item">
+                    <li class="nav-item">
                         <a class="nav-link" id="structure-tab" data-bs-toggle="pill" href="#structure" role="tab">Latitude</a>
                     </li>
-                     <li class="nav-item">
+                    <li class="nav-item">
                         <a class="nav-link" id="structure-tab" data-bs-toggle="pill" href="#structure" role="tab">Modification</a>
                     </li>
                     <li class="nav-item">
@@ -46,73 +44,129 @@
                     </li> -->
                 </ul>
             </div>
-
             <div class="col-lg-10">
                 <!-- Tab Content -->
                 <div class="tab-content" id="inspectorateTabsContent">
                     <!-- Tab 1 -->
                     <div class="tab-pane fade show active" id="org" role="tabpanel">
-                        <div class="alert alert-info">
-                            <h5><strong>THE ORGANIZATION</strong></h5>
+                        <div class="row text-center">
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-building fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary"><a href="services-checklist-ae.php">AE </a></h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-people fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary">AO</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-journal-text fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary">AA</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-calendar-event fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary">GE</h6>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur quisquam quis est quod mollitia. Culpa veniam, facilis, incidunt numquam obcaecati temporibus quis qui maiores nulla odio magnam. A deserunt cum vitae provident quas rerum ea explicabo mollitia, labore neque commodi aperiam asperiores. Maxime, aut aperiam nulla molestiae ipsam odit, rerum esse laborum exercitationem, animi autem impedit eligendi optio perspiciatis est! Mollitia, error similique. Consequuntur veniam ab nesciunt rem aliquid aliquam sequi maiores excepturi, iste tenetur. Sed, illum? Ex, labore! Explicabo officiis perferendis id deleniti similique ratione soluta veritatis blanditiis. Pariatur aperiam quasi sunt esse? Illo possimus animi recusandae architecto maxime, obcaecati consectetur dolorum. Voluptatem optio perferendis aliquid veniam? Officia, corrupti. Numquam, officiis! Corrupti dolorem saepe rem nihil quas nam quisquam, provident, labore distinctio blanditiis modi facere aliquam iusto assumenda expedita ducimus deserunt delectus cupiditate vero corporis amet. Fugit ea deserunt, ipsam laborum itaque atque dicta dolores, dolore expedita adipisci porro distinctio inventore officia explicabo nulla. Possimus et mollitia blanditiis. Earum, dolorum omnis nam eius quisquam laborum tenetur accusamus velit ullam, magnam a facere aspernatur tempore deleniti quos explicabo ea possimus corrupti consequuntur totam ad enim. Dignissimos facere exercitationem quidem veniam praesentium eos? Minima accusamus temporibus officia nesciunt assumenda voluptate velit dignissimos fugiat libero eligendi? Expedita non, reiciendis porro nesciunt libero quaerat, quidem repellat ad, maxime nihil hic animi ducimus doloribus eveniet eaque quisquam culpa alias voluptatem minus iusto magnam laboriosam. Eos voluptatem tempora asperiores neque maiores, autem deleniti quo blanditiis impedit iste voluptatum, ea sapiente? Quos id similique magnam quae.</p>
-                    </div>
-
-                    <!-- Tab 2 -->
-                    <div class="tab-pane fade" id="functions" role="tabpanel">
-                        <div class="alert alert-info">
-                            <h5><strong>FUNCTIONS</strong></h5>
+                        <div class="row text-center">
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-building fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary">EE</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-people fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary">BE</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card border border-primary">
+                                    <div class="card-body">
+                                        <i class="bi bi-calendar-event fs-2 text-primary"></i>
+                                        <h6 class="mt-2 text-primary">Events</h6>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <p><strong>ROLE:</strong></p>
-                        <ul>
-                            <li>Advise commanders...</li>
-                            <!-- More items -->
-                        </ul>
-                        <p><strong>STATUS:</strong></p>
-                        <ul>
-                            <li>Advisory in the application of flight safety...</li>
-                        </ul>
-                        <p><strong>DIVISION OF WORK:</strong></p>
-                        <ul>
-                            <li>Remedial (including reporting and investigation).</li>
-                            <!-- More items -->
-                        </ul>
                     </div>
-
-                    <!-- Tab 3 -->
-                    <div class="tab-pane fade" id="structure" role="tabpanel">
-                        <div class="alert alert-info">
-                            <h5><strong>ORGANIZATION</strong></h5>
-                        </div>
-                        <img class="img-fluid" src="assets/img/about/organisation.jpg" alt="Organization Chart">
+                </div>
+            
+            <!-- Tab 2 -->
+                <div class="tab-pane fade" id="functions" role="tabpanel">
+                    <div class="alert alert-info">
+                        <h5><strong>FUNCTIONS</strong></h5>
                     </div>
+                    <p><strong>ROLE:</strong></p>
+                    <ul>
+                        <li>Advise commanders...</li>
+                        <!-- More items -->
+                    </ul>
+                    <p><strong>STATUS:</strong></p>
+                    <ul>
+                        <li>Advisory in the application of flight safety...</li>
+                    </ul>
+                    <p><strong>DIVISION OF WORK:</strong></p>
+                    <ul>
+                        <li>Remedial (including reporting and investigation).</li>
+                        <!-- More items -->
+                    </ul>
+                </div>
 
-                    <!-- Tab 4 -->
-                    <div class="tab-pane fade" id="responsibilities" role="tabpanel">
-                        <div class="alert alert-info">
-                            <h5><strong>RESPONSIBILITIES</strong></h5>
-                        </div>
-                        <h6>Responsibilities of Command Flight Safety</h6>
-                        <ul>
-                            <li>Conducting visits to flying units...</li>
-                            <!-- More items -->
-                        </ul>
-
-                        <h6 class="mt-4">Responsibilities of Base/Station Flight Safety</h6>
-                        <ul>
-                            <li>Assisting the Base/Station commander...</li>
-                            <!-- More items -->
-                        </ul>
-
-                        <h6 class="mt-4">Responsibilities of Squadron Flight Safety</h6>
-                        <ul>
-                            <li>Promoting high standards of risk awareness...</li>
-                            <!-- More items -->
-                        </ul>
+                <!-- Tab 3 -->
+                <div class="tab-pane fade" id="structure" role="tabpanel">
+                    <div class="alert alert-info">
+                        <h5><strong>ORGANIZATION</strong></h5>
                     </div>
+                    <img class="img-fluid" src="assets/img/about/organisation.jpg" alt="Organization Chart">
+                </div>
+
+                <!-- Tab 4 -->
+                <div class="tab-pane fade" id="responsibilities" role="tabpanel">
+                    <div class="alert alert-info">
+                        <h5><strong>RESPONSIBILITIES</strong></h5>
+                    </div>
+                    <h6>Responsibilities of Command Flight Safety</h6>
+                    <ul>
+                        <li>Conducting visits to flying units...</li>
+                        <!-- More items -->
+                    </ul>
+
+                    <h6 class="mt-4">Responsibilities of Base/Station Flight Safety</h6>
+                    <ul>
+                        <li>Assisting the Base/Station commander...</li>
+                        <!-- More items -->
+                    </ul>
+
+                    <h6 class="mt-4">Responsibilities of Squadron Flight Safety</h6>
+                    <ul>
+                        <li>Promoting high standards of risk awareness...</li>
+                        <!-- More items -->
+                    </ul>
                 </div>
             </div>
         </div>
+    </div>
         <hr>
     </main>
     <?php
---
+++ services/checklist-ae.php
@@ -0,0 +1,109 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include '../template/head.php';
+
+require_once "../includes/config.php";
+
+// Fetch document details
+$stmt = $db->prepare("SELECT d.title,d.description, b.name AS branch_name , d.uploaded_by ,c.name AS category, d.uploaded_at, d.file_path FROM documents d
+JOIN categories c ON d.category_id = c.id
+JOIN branches b ON d.branch_id = b.id
+ WHERE d.category_id = 7 AND d.is_active = 1 AND d.branch_id = 6 ORDER BY d.uploaded_at DESC");
+
+$stmt->execute();
+$result = $stmt->get_result();
+?>
+
+
+<body>
+    <?php 
+    include '../template/header.php';
+    ?>
+
+
+    <!-- Main Content -->
+    <main class="container my-5 pt-5">
+        <div class="page-header mb-4">
+            <h3 class="colour-defult">Services <i class="fa fa-tasks"></i>
+                <div class="float-end">
+                    <a href="index.php" class="btn btn-info me-2"><i class="fa fa-home"></i> Home</a>
+                    <a href="javascript:history.back()" class="btn btn-secondary"><i class="fa fa-arrow-left"></i> Back</a>
+                </div>
+            </h3>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-2">
+                <!-- Nav Tabs -->
+                <ul class="nav flex-column nav-pills" id="inspectorateTabs" role="tablist" aria-orientation="vertical">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="org-tab" data-bs-toggle="pill" href="#org" role="tab">Check List</a>
+                    </li>
+                    
+                </ul>
+            </div>
+
+            <div class="col-lg-10">
+                <table class="table table-bordered text-center align-middle">
+                    <thead class="table-primary">
+                        <tr>
+                            <th>Title</th>
+                            <th>description</th>
+                            <!-- <th>Branch</th> -->
+                            <!-- <th>Category</th> -->
+                            <!-- <th>Uploaded By</th> -->
+                            <th>Uploaded Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if ($result->num_rows > 0): ?>
+                            <?php while ($row = $result->fetch_assoc()): ?>
+                                <tr>
+                                    <td><?= htmlspecialchars($row['title']) ?></td>
+                                    <td><?= htmlspecialchars($row['description']) ?></td>
+                                    <!-- <td><?= htmlspecialchars($row['branch_name']) ?></td> -->
+                                    <!-- <td><?= htmlspecialchars($row['category']) ?></td> -->
+                                    <!-- <td><?= htmlspecialchars($row['uploaded_by']) ?></td> -->
+                                    <td><?= htmlspecialchars($row['uploaded_at']) ?></td>
+                                    <td>
+                                        <a href="admin/action/<?= htmlspecialchars($row['file_path']) ?>" target="_blank" class="btn btn-sm btn-primary">View</a>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        <?php else: ?>
+                            <tr>
+                                <td colspan="5">No documents found</td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- Repeat for 6 more cards -->
+        </div>
+
+
+
+        </div>
+
+       
+
+     
+
+    
+    
+        <hr>
+    </main>
+    <?php
+    include '../template/footer.php';
+    include '../template/foot.php';
+    ?>
+
+</body>
+
+</html>
