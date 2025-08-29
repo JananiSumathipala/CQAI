7b3fd7e4b9c56eaf78cdec84aa9e66c21b463ea5
J.M.Sumathipala <149936991+JananiSumathipala@users.noreply.github.com>
2025-08-29 15:07:45 +0530

    publications

---
+++ publication/online_sub.php
@@ -0,0 +1,64 @@
+<?php include '../template/head.php'; ?>
+<head>
+    <link rel="stylesheet" href="../assets/css/audit_card.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+<body>
+    <?php include '../template/header.php'; ?>
+    
+    <!-- Main Content -->
+    <main class="container my-5 pt-5">
+        <div class="page-header mb-4">
+            <h3 class="colour-defult">Online Subscription<i class="fa fa-tasks"></i>
+                <div class="float-end">
+                    <a href="../index.php" class="btn btn-info me-2"><i class="fa fa-home"></i> Home</a>
+                    <a href="javascript:history.back()" class="btn btn-secondary"><i class="fa fa-arrow-left"></i> Back</a>
+                </div>
+            </h3>
+        </div>
+        
+        <div class="row">
+
+            <!-- Right Side Content -->
+            <div class="col-lg- col-2xl-10">
+                <div class="tab-content" id="inspectorateTabsContent">
+                    
+ 
+                </div>
+            </div>
+        </div>
+        <hr>
+    </main>
+
+    <?php
+    include '../template/footer.php';
+    include '../template/foot.php';
+    ?>
+    
+    <!-- Bootstrap JS Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script>
+        // Handle URL hash for direct tab access
+        document.addEventListener("DOMContentLoaded", function() {
+            var hash = window.location.hash;
+            if (hash) {
+                var tabTrigger = document.querySelector('a[href="' + hash + '"]');
+                if (tabTrigger) {
+                    var tab = new bootstrap.Tab(tabTrigger);
+                    tab.show();
+                }
+            }
+            
+            // Update URL hash when tabs are shown
+            var tabEls = document.querySelectorAll('a[data-bs-toggle="pill"]');
+            tabEls.forEach(function(tabEl) {
+                tabEl.addEventListener('shown.bs.tab', function (e) {
+                    history.replaceState(null, null, e.target.getAttribute('href'));
+                });
+            });
+        });
+    </script>
+</body>
+</html>
